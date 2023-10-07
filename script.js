25b20b28ca92ea563e86112193595c167678a951
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2023-10-07 13:52:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// if-else by using comparison operator
+
+let age = 60
+if(age>=61)
+{
+  console.log("Your are eligible for the special discount")
+
+}
+else{
+  console.log("your are not eligible")
+}
+console.log("Happy shopping")
+
+//output
+// - your are not eligible
+// - Happy shopping
